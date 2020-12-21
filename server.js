d9b15e7b26428429f994e7ab0aae0ea3af4cbfa2
prof4 <52273992+prof4@users.noreply.github.com>
2020-12-21 13:26:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
+const path = require("path")
 
 const rooms = {};
 
@@ -34,6 +35,13 @@ io.on("connection", socket => {
         io.to(incoming.target).emit("ice-candidate", incoming.candidate);
     });
 });
+
+if (){
+    app.use(express.static(path.join(__dirname, './client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './client/build/index.html')));
+    }
+}
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`server is running on port ${port}`));
